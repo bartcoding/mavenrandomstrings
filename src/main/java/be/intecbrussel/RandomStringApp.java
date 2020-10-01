02af4f94e13111d9f7686cdd5522ad0b1d183425
BartIntec <bart.hunerbein@intecbrussel.be>
2020-10-01 10:51:24 +0200

    Initial commit

---
+++ src/main/java/be/intecbrussel/RandomStringApp.java
@@ -0,0 +1,22 @@
+package be.intecbrussel;
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class RandomStringApp{
+
+
+
+public static void main(String[] args){
+
+	String random = RandomStringUtils.randomAlphabetic(10);
+	System.out.println(random);
+}
+
+
+
+
+
+
+
+
+
+}
